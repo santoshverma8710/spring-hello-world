116b2bf73d32855d68fe5f1e3877b282695845a4
santoshverma8710 <50474494+santoshverma8710@users.noreply.github.com>
2019-08-26 10:27:28 +0530

    Update the code

--- src/main/java/com/demo/spring/helloworld/springhelloworld/DemoApplication.java
+++ src/main/java/com/demo/spring/helloworld/springhelloworld/DemoApplication.java
@@ -2,12 +2,20 @@ package com.demo.spring.helloworld.springhelloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@RequestMapping("/hello")
+	public String hello()
+	{
+		return "Hello World";
+	}
 }
